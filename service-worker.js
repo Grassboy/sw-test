9af546d78df704f8caf91bebc4ccee07744bc7c1
grassboy <grassboy@github.com>
2016-07-01 18:03:58 +0800

    test

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,31 @@
+var VERSION = 'v1';
 self.addEventListener('install', function(e){
     console.log('service worker installed');
+    e.waitUntil(
+        caches.open(VERSION).then(function(cache){
+            return cache.addAll([
+                '/sw-test/',
+                '/sw-test/index.html',
+                '/sw-test/gallery/bountyHunters.jpg'
+            ]);
+        });
+    );
+});
+self.addEventListener('fetch', function(e){
+    console.log('Start Fetch with URL: ', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(rsp){
+            if(!rsp) {
+                console.log('Cache Missed: ', e.request.url);
+            }
+            return rsp || fetch(e.request).then(function(response){
+                cosnole.log('loaded from internet', e.request.url);
+                return caches.open(VERSION).then(function(cache){
+                    cache.put(e.request, response.clone());
+                    console.log('got it from internet');
+                    return response;
+                });
+            });
+        })
+    )
 });
